b521f3da50e43da3c07e5b1aa4fda985d4621211
Vraj Shah <thevrajshah@gmail.com>
2020-10-04 16:30:01 +0530

    Updates

---
+++ precache-manifest.72c98d40570add34dc2b8d9217462b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52199b78f54216ea125860f619c86d9c",
+    "url": "/shelterhero/index.html"
+  },
+  {
+    "revision": "4c5d74df743f792c022a",
+    "url": "/shelterhero/static/css/main.17e379c0.chunk.css"
+  },
+  {
+    "revision": "8932413d0b8ff8d0581e",
+    "url": "/shelterhero/static/js/2.49edfffe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shelterhero/static/js/2.49edfffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5d74df743f792c022a",
+    "url": "/shelterhero/static/js/main.56ed1034.chunk.js"
+  },
+  {
+    "revision": "d8aaaaea5e981e864ac8",
+    "url": "/shelterhero/static/js/runtime-main.14a98679.js"
+  },
+  {
+    "revision": "5405d77c51fb46a0cbf26cb96fe4da4d",
+    "url": "/shelterhero/static/media/user.5405d77c.png"
+  }
+]);
